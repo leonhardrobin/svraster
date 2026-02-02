169308d3a370aa7a6f92d9e87ba52b597051a0fc
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2026-02-02 11:13:45 +0000

    delete comment

--- custom_scripts/run_batch_mesh_extraction.sh
+++ custom_scripts/run_batch_mesh_extraction.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-# ================= CONFIGURATION =================
-# Set the base absolute path to your project root
 PROJECT_ROOT="/nfs/lschnaitl/projects/svraster"
 
-# Define the list of directories you want to train on
 TARGET_DIRS=("chair" "drums" "ficus" "hotdog" "lego" "materials" "mic" "ship")
-# =================================================
 
-# Loop through each directory
 for dir_name in "${TARGET_DIRS[@]}"; do
-    # Construct the specific paths based on the directory name
     MODEL_PATH="output/nerf_synthetic/${dir_name}"
 
     echo "------------------------------------------------"
     echo "Starting mesh extraction for: $dir_name"
     echo "Model Output: $MODEL_PATH"
 
-    # Check if the processed data actually exists before trying to train
     if [ -d "$MODEL_PATH" ]; then
         python extract_mesh.py $MODEL_PATH \
             --use_vert_color \
